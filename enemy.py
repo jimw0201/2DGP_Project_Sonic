bb4475d6351bc329ecaf31aed9a9c38fcee18212
minwooji <jimw0201@gmail.com>
2024-12-01 03:25:36 +0900

    적 스프라이트 이미지 수정 / Burrobot 적 추가

--- enemy.py
+++ enemy.py
@@ -17,6 +17,7 @@ TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION_CRABMEAT = 3
 FRAMES_PER_ACTION_CATERKILLER = 7
+FRAMES_PER_ACTION_BURROBOT = 2
 
 class Crabmeat:
     images = None
@@ -97,4 +98,44 @@ class Caterkiller:
     def get_bb(self):
         width = 102 // 2
         height = 66 // 2
+        return self.x - width, self.y - height, self.x + width, self.y + height
+
+class Burrobot:
+    images = None
+
+    def __init__(self, sonic):
+        self.x, self.y = random.randint(1700, 2600), 120
+        self.image = load_image('enemies_sprite_nbg.png')
+        self.frame = 0
+        self.dir = random.choice([-1, 1])
+        self.sonic = sonic
+        self.attack_sound = pygame.mixer.Sound('attack.mp3')
+        self.attack_sound.set_volume(0.5)
+
+    def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION_CRABMEAT * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION_BURROBOT
+        self.x += RUN_SPEED_PPS * self.dir * game_framework.frame_time
+        if self.x <= 50 or self.x >= 19150:
+            self.dir *= -1
+
+        if random.random() < 0.01:
+            self.dir *= -1
+
+    def handle_collision(self, group, other):
+        if group == 'sonic:burrobot' and other.is_jumping:
+            self.attack_sound.play()
+            game_world.remove_object(self)
+            play_mode.score += 100
+
+    def draw(self, camera_x):
+        if self.dir == 1:
+            self.image.clip_draw(int(self.frame) * 28, 1329, 28, 35, self.x - camera_x, self.y, 56, 70)
+        else:
+            self.image.clip_draw(int(self.frame) * 28, 1288, 28, 35, self.x - camera_x, self.y, 56, 70)
+        left, bottom, right, top = self.get_bb()
+        draw_rectangle(left - camera_x, bottom, right - camera_x, top)
+
+    def get_bb(self):
+        width = 56 // 2
+        height = 70 // 2
         return self.x - width, self.y - height, self.x + width, self.y + height
--- play_mode.py
+++ play_mode.py
@@ -2,7 +2,7 @@ from pico2d import *
 import game_framework
 
 import game_world
-from enemy import Crabmeat, Caterkiller
+from enemy import Crabmeat, Caterkiller, Burrobot
 from ground import Ground, Background
 from ring import Ring
 from sonic import Sonic
@@ -35,7 +35,7 @@ def handle_events():
                 sonic.handle_event(event)
 
 def init():
-    global camera_x, bgm, jump_sound, ground, background, sonic, rings, font, enemies, life_display, lives
+    global camera_x, bgm, jump_sound, ground, background, sonic, rings, font, enemies, enemies2, enemies3, life_display, lives
     camera_x = 0
 
     font = load_font('NiseSegaSonic.TTF', 20)
@@ -48,6 +48,7 @@ def init():
     life_display = LifeDisplay(lives)
     enemies = [Crabmeat(sonic) for _ in range(5)]
     enemies2 = [Caterkiller(sonic) for _ in range(5)]
+    enemies3 = [Burrobot(sonic) for _ in range(3)]
 
     rings = [Ring(300 + i * 100, 300, sonic) for i in range(10)]
     game_world.add_objects(rings, 3)
@@ -58,6 +59,7 @@ def init():
     game_world.add_object(sonic, 2)
     game_world.add_objects(enemies, 2)
     game_world.add_objects(enemies2, 2)
+    game_world.add_objects(enemies3, 2)
 
     # 충돌 체크 그룹
     for enemy in enemies:
@@ -65,6 +67,9 @@ def init():
 
     for enemy in enemies2:
         game_world.add_collision_pair(sonic, enemy, 'sonic:caterkiller')
+
+    for enemy in enemies3:
+        game_world.add_collision_pair(sonic, enemy, 'sonic:burrobot')
 
     # 배경음악
     bgm = load_music('green_hill_zone_bgm.mp3')
